0b8b34b2b0f23e695afadbad147798bcd0ddd859
Joshua Calloway <joshua.calloway@gmail.com>
2016-03-18 12:35:10 -0500

    add more aws env variables

--- setenv.sh
+++ setenv.sh
@@ -3,3 +3,5 @@
 export PATH=$(pwd):$PATH
 export TF_VAR_aws_access_key=$(findAccessKey.sh awsboot)
 export TF_VAR_aws_secret_key=$(findSecretKey.sh $TF_VAR_aws_access_key)
+export AWS_ACCESS_KEY_ID=$(findAccessKey.sh awsboot)
+export AWS_SECRET_ACCESS_KEY=$(findSecretKey.sh $TF_VAR_aws_access_key)
