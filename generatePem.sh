722853975be5d27bb82742665d19b29c271af5b5
Joshua Calloway <joshua.calloway@gmail.com>
2016-03-19 22:22:27 -0500

    fixed error in generating pem

--- generatePem.sh
+++ generatePem.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 aws ec2 delete-key-pair --key-name --region us-west-2 awsboot
-aws ec2 create-key-pair --key-name awsboot --region us-west-2 > /root/.aws/awsboot.pem
+aws ec2 create-key-pair --key-name awsboot --output json --region us-west-2 | jq -r ".KeyMaterial" > /root/.aws/awsboot.pem
