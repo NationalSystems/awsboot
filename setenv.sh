b47d1403b90ba01293ddc8aab0e7c45188b0fd2a
Joshua Calloway <joshua.calloway@gmail.com>
2016-03-20 16:48:07 -0500

    can now take arbitrary profile

--- setenv.sh
+++ setenv.sh
@@ -2,5 +2,6 @@
 
 profile=$1
 export PATH=$(pwd):$PATH
-export TF_VAR_aws_access_key=$(findAccessKey.sh jcalloway)
+export TF_VAR_aws_access_key=$(findAccessKey.sh $profile)
 export TF_VAR_aws_secret_key=$(findSecretKey.sh $TF_VAR_aws_access_key)
+
