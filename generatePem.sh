cb8d945d5ba11fdf3c45a037de977ba2a877d191
Joshua Calloway <joshua.calloway@gmail.com>
2016-03-20 16:48:36 -0500

    now generates parameter appname pem

--- generatePem.sh
+++ generatePem.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
-aws ec2 delete-key-pair --key-name --region us-west-2 awsboot
-aws ec2 create-key-pair --key-name awsboot --output json --region us-west-2 | jq -r ".KeyMaterial" > /root/.aws/awsboot.pem
+appname=$1
+
+aws ec2 delete-key-pair --key-name --region us-west-2 $appname
+aws ec2 create-key-pair --key-name $appname --output json --region us-west-2 | jq -r ".KeyMaterial" > ~/.aws/${appname}.pem
