da575472276499a85ed70ec5bc65235c7f3bc497
Joshua Calloway <joshua.calloway@gmail.com>
2016-03-19 20:56:47 -0500

    don't export AWS_ACCESS env variables

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
+profile=$1
 export PATH=$(pwd):$PATH
-export TF_VAR_aws_access_key=$(findAccessKey.sh awsboot)
+export TF_VAR_aws_access_key=$(findAccessKey.sh jcalloway)
 export TF_VAR_aws_secret_key=$(findSecretKey.sh $TF_VAR_aws_access_key)
-export AWS_ACCESS_KEY_ID=$(findAccessKey.sh awsboot)
-export AWS_SECRET_ACCESS_KEY=$(findSecretKey.sh $TF_VAR_aws_access_key)
