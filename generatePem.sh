c66956eaac8826b607da067c6b3237a6caa68a22
Joshua Calloway <joshua.calloway@gmail.com>
2016-03-21 14:23:05 -0500

    fix script errors

--- generatePem.sh
+++ generatePem.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-appname=$1
+awsprofile=$1
+appname=$2
 
-aws ec2 delete-key-pair --key-name --region us-west-2 $appname
-aws ec2 create-key-pair --key-name $appname --output json --region us-west-2 | jq -r ".KeyMaterial" > ~/.aws/${appname}.pem
+aws --profile $awsprofile ec2 delete-key-pair --key-name $appname
+aws --profile $awsprofile ec2 create-key-pair --key-name $appname --output json | jq -r ".KeyMaterial" > ~/.aws/${appname}.pem
--- setenv.sh
+++ setenv.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
-profileName=$1
-userName=$2
+awsprofile=$1
+awsuser=$2
 appname=$3
 dnsname=$4
 export PATH=$(pwd):$PATH
-export TF_VAR_aws_access_key=$(findAccessKey.sh $profileName $userName)
+export TF_VAR_aws_access_key=$(findAccessKey.sh $awsprofile $awsuser)
 export TF_VAR_aws_secret_key=$(findSecretKey.sh $TF_VAR_aws_access_key)
 export TF_VAR_appname=$appname
 
 export TF_VAR_dnsdomain=$dnsdomain
-./generatePem.sh $TF_VAR_appname
+./generatePem.sh $awsprofile $TF_VAR_appname
 export TF_VAR_awsboot_pem=`cat ~/.aws/${TF_VAR_appname}.pem` 
 export TF_VAR_aws_route53_zone_id=`aws route53 list-hosted-zones-by-name \
                    --dns-name $TF_VAR_dnsdomain --query HostedZones[0].Id | \
