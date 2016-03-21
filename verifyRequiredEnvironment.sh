eab3f87c2929d9cc4b5c08c861eaa054df3c6e99
Joshua Calloway <joshua.calloway@gmail.com>
2016-03-20 21:06:54 -0500

    script holder to verify env

---
+++ verifyRequiredEnvironment.sh
@@ -0,0 +1,6 @@
+
+#
+# TODO fix this, so we can verify that derived images have the right environment variables
+#
+#if [ "x$appname" = "x" ]; then echo "Need ENV appname defined" && exit 1; fi
+#if [ "x$dnsdomain" = "x" ]; then echo "Need ENV dnsdomain defined" && exit 1; fi
