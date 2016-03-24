c7928b086fb31b6f930ac3178b4fd85289cc4a5d
Joshua Calloway <joshua.calloway@gmail.com>
2016-03-24 17:00:23 -0500

    fixes go generate pem and find hosted zone id

--- setenv.sh
+++ setenv.sh
@@ -3,17 +3,19 @@
 awsprofile=$1
 awsuser=$2
 appname=$3
+awsregion=$4
 dnsname=$4
 export PATH=$(pwd):$PATH
 export TF_VAR_aws_access_key=$(findAccessKey.sh $awsprofile $awsuser)
 export TF_VAR_aws_secret_key=$(findSecretKey.sh $TF_VAR_aws_access_key)
 export TF_VAR_appname=$appname
+export TF_VAR_aws_region=$awsregion
 
 export TF_VAR_dnsdomain=$dnsdomain
 export TF_VAR_awsinstancetype=${awsinstancetype:-t2.micro}
-./generatePem.sh $awsprofile $TF_VAR_appname
+./generatePem.sh $awsprofile $appname
 export TF_VAR_awspem=`cat ~/.aws/${TF_VAR_appname}.pem`
 export TF_VAR_aws_route53_zone_id=`aws --profile $awsprofile route53 list-hosted-zones-by-name \
                    --dns-name $TF_VAR_dnsdomain --query HostedZones[0].Id | \
-                    sed 's/\/hostedzone\///'` && \
+                    sed 's/\/hostedzone\///' | sed 's/"//g'` 
 chmod 400 ~/.aws/${TF_VAR_appname}.pem
