f567e81d98f237f5b75d5b12f19ca6b3745316c2
Joshua Calloway <joshua.calloway@gmail.com>
2016-04-06 11:12:45 -0500

    make backup copy of pem

--- generatePem.sh
+++ generatePem.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
 appname=$1
-
-rm -rf ~/.aws/${appname}.pem
+cp ~/.aws/${appname}.pem{,.bak}
 
 aws --profile $awsprofile ec2 delete-key-pair --key-name $appname
 aws --profile $awsprofile ec2 create-key-pair --key-name $appname --output json | jq -r ".KeyMaterial" > ~/.aws/${appname}.pem
