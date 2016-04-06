d310824073648b9ad06c72e934533d8ba409c941
Matthew Long <mlong@rozoom.com>
2016-04-06 11:19:46 -0500

    updated us-west-1 ami image

--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "aws_access_key" {}
 variable "aws_region" {}
 variable "amis" {
   default = {
-    eu-west-1 = "ami-1b0f7d7b"
+    us-west-1 = "ami-1b0f7d7b"
     us-west-2 = "ami-c229c0a2"
     us-east-1 = "ami-8fcee4e5" 
   }
